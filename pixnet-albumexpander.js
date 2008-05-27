5ea5572a97e76895a443921012d522c0520290e0
gslin@gslin.org <gslin@gslin.org>
2008-05-28 06:06:24 +0800

    - Use Google Ajax Libraries.

--- pixnet-albumexpander.js
+++ pixnet-albumexpander.js
@@ -26,8 +26,8 @@ function GM_wait()
 }
 
 var GM_JQ = document.createElement('script');
-GM_JQ.src = 'http://s1.pixfs.net/js/jquery/jquery-1.2.3/jquery-1.2.3.pack.js';
+GM_JQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js';
 GM_JQ.type = 'text/javascript';
-document.getElementsByTagName("head")[0].appendChild(GM_JQ);
+document.getElementsByTagName('head')[0].appendChild(GM_JQ);
 
 GM_wait();
--- wretch-albumexpander.js
+++ wretch-albumexpander.js
@@ -8,31 +8,31 @@
 
 function album_expander($)
 {
-    var imgs = "";
+    var imgs = '';
 
-    $(".side").each(function() {
-	    var a = $("a", this);
-	    var alink = a.attr("href");
-	    var img = $("img", this);
-	    var imgsrc = img.attr("src").replace(/\/thumbs\/t?/, "/");
+    $('.side').each(function() {
+	    var a = $('a', this);
+	    var alink = a.attr('href');
+	    var img = $('img', this);
+	    var imgsrc = img.attr('src').replace(/\/thumbs\/t?/, '/');
 
-	    imgs += "<a href='" + alink + "'><img alt='' src='" + imgsrc + "'></a><br />";
+	    imgs += '<a href="' + alink + '"><img alt="" src="' + imgsrc + '"></a><br />';
 	});
-    $("table").slice(3,4).html(imgs);
+    $('table').slice(3,4).html(imgs);
 }
 
 function GM_wait()
 {
-    if (typeof unsafeWindow.jQuery == "undefined")
+    if (typeof unsafeWindow.jQuery == 'undefined')
 	window.setTimeout(GM_wait, 100);
     else
 	album_expander(unsafeWindow.jQuery);
 }
 
-var GM_JQ = document.createElement("script");
-GM_JQ.src = "http://code.jquery.com/jquery-1.2.3.js";
-GM_JQ.type = "text/javascript";
-document.getElementsByTagName("head")[0].appendChild(GM_JQ);
+var GM_JQ = document.createElement('script');
+GM_JQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js';
+GM_JQ.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(GM_JQ);
 
 GM_wait();
 
